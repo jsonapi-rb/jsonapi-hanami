de4adb7ee0fa852d102866dddc5ca7cd594d34cc
Lucas Hosseini <lucas.hosseini@gmail.com>
2017-05-18 23:14:34 +0200

    Refactor deserialization/parsing.

--- lib/jsonapi/hanami/deserialization.rb
+++ lib/jsonapi/hanami/deserialization.rb
@@ -3,6 +3,8 @@ require 'jsonapi/deserializable'
 module JSONAPI
   module Hanami
     module Deserialization
+      REVERSE_MAPPING_KEY = 'jsonapi_deserializable.reverse_mapping'.freeze
+
       def self.included(base)
         base.extend ClassMethods
       end
@@ -21,34 +23,12 @@ module JSONAPI
         # @api private
         def _deserializable(key, options, fallback, &block)
           klass = options[:class] || Class.new(fallback, &block)
-          use DeserializationMiddleware, key, klass
-        end
-      end
 
-      class DeserializationMiddleware
-        ROUTER_PARAMS = 'router.params'.freeze
-        ROUTER_PARSED_BODY = 'router.parsed_body'.freeze
-        JSONAPI_KEYS = [:data, :meta, :links, :jsonapi].freeze
-
-        def initialize(app, key, klass)
-          @app = app
-          @deserializable_key = key
-          @deserializable_class = klass
-        end
-
-        def call(env)
-          body = env[ROUTER_PARSED_BODY]
-          deserialized_hash = @deserializable_class.call(body)
-          params = env[ROUTER_PARAMS]
-          # TODO(beauby): Actually replace the request body upstream instead
-          #   of hacking it here.
-          params[:_jsonapi] = {}
-          JSONAPI_KEYS.each do |key|
-            params[:_jsonapi][key] = params.delete(key) if params.key?(key)
+          before do |params|
+            resource = klass.new(params[:_jsonapi])
+            params.env[REVERSE_MAPPING_KEY] = resource.reverse_mapping
+            params[key.to_sym] = resource.to_hash
           end
-          params[@deserializable_key] = deserialized_hash
-
-          @app.call(env)
         end
       end
     end
